410f4a6946728d8b16cfabe599804fe4db36719c
Priscila <pnmelo2808@gmail.com>
2024-11-06 17:39:02 -0300

    0.12 - Prisma Config

--- src/app.js
+++ src/app.js
@@ -12,14 +12,22 @@ const prisma = new PrismaClient({
   },
 });
 
+prisma.$connect()
+  .then(() => {
+    console.log('Conectado ao banco de dados');
+  })
+  .catch((error) => {
+    console.error('Erro ao conectar ao banco de dados:', error);
+  });
+
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-
 app.use('/users', userRoutes);
 app.use('/emotions', emotionRoutes);
 
-app.listen(3000, () => {
-  console.log('Servidor rodando na porta 3000');
+const port = process.env.PORT || 3000; // Porta dinâmica
+app.listen(port, () => {
+  console.log(`Servidor rodando na porta ${port}`);
 });
